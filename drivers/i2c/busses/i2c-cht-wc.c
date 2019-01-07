ff3c761195798ae37e3ed79f818c2ff92bd58617
Yauhen Kharuzhy <jekhor@gmail.com>
2019-01-07 20:22:30 +0300

    i2c-cht-wc bus: Pass wait-for-usbphy property to bq25892 charger
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- drivers/i2c/busses/i2c-cht-wc.c
+++ drivers/i2c/busses/i2c-cht-wc.c
@@ -291,6 +291,7 @@ static const struct property_entry bq24192_props[] = {
 	PROPERTY_ENTRY_U32("ti,input-max-current", 1950000),
 //	PROPERTY_ENTRY_BOOL("ti,boost-low-freq"),
 	PROPERTY_ENTRY_BOOL("disable-reset"),
+	PROPERTY_ENTRY_BOOL("wait-for-usbphy"),
 	{}
 };
 
